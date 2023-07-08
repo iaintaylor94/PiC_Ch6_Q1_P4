eeab1df246b3ea3ef13e34cdd45660d76b9206b4
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-08 13:00:01 +0000

    completed even/odd Ver. 2

--- .ccls-cache/@home@runner@Ch6Q1P4/main.c
+++ .ccls-cache/@home@runner@Ch6Q1P4/main.c
@@ -1,6 +1,21 @@
+// Program to determine if a number is even or odd (Ver. 2)
+
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  int numberToTest, remainder;
+
+  printf ("Enter your number to be tester: ");
+  scanf ("%d", &numberToTest);
+
+  remainder = numberToTest % 2;
+
+  if (remainder == 0) {
+    printf ("The unmber is even\n");
+  }
+  else {
+    printf ("The number is odd\n");
+  }
+  
   return 0;
 }
